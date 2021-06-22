a611a47cc43d7bb8ac44193e2f11e9b8c940aed9
Matheus <math.cs.ceil@gmail.com>
2021-06-22 11:19:19 -0300

    #1905

--- src/pages/Sis/Dashboard/index.js
+++ src/pages/Sis/Dashboard/index.js
@@ -28,7 +28,7 @@ export default function Dashboard() {
   const { users } = useUser();
   const { users: researches } = useResearcher();
   const { programs, status } = useProgram();
-  const { evaluators } = useEvaluator();
+  const { evaluators, evaluations } = useEvaluator();
 
   const { review } = useProject();
 
@@ -177,6 +177,31 @@ export default function Dashboard() {
           </div>
         </CardDashboard>
       </div>
+
+      {user.profile.name == 'Pesquisador'
+      && (
+      <div className="col-3 px-0">
+        <CardDashboard
+          style={{ cursor: 'pointer' }}
+          onClick={() => {
+            history.push('avaliacoes');
+          }}
+          className="green"
+        >
+          <div className="card-body">
+            <div className="row">
+              <div className="col">
+                <div className="title">Avaliações</div>
+                <div className="number pulsate">{evaluations.length}</div>
+              </div>
+              <div className="col-auto">
+                <FiFolder size="3em" />
+              </div>
+            </div>
+          </div>
+        </CardDashboard>
+      </div>
+      )}
 
       {user.profile.name == 'Administrador'
       && (
