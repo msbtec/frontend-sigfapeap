64f8f6871a14e8f04f898a876a0378ce79188e17
Matheuscsceil <math.cs.ceil@gmail.com>
2021-06-27 15:27:42 -0300

    ajuste

--- src/pages/private/EvaluatorsProgram/Avaliador/index.js
+++ src/pages/private/EvaluatorsProgram/Avaliador/index.js
@@ -1,10 +1,8 @@
 import React, { useState, useEffect } from 'react';
 
-import { FiEdit, FiTrash } from 'react-icons/fi';
+import { FiEdit } from 'react-icons/fi';
 
 import ReactTooltip from 'react-tooltip';
-
-import { useResearcher } from '../../../../hooks/researcher';
 
 import { StyledModal } from './styles';
 import { Card } from '../../../../components/Card';
@@ -13,6 +11,8 @@ import { Table } from '../../../../components/Table';
 
 import FiltersEvaluator from '../../../../components/FiltersEvaluator';
 import api from '../../../../services/api';
+
+import Solicitacao from '../Solicitação';
 
 export default function EnviarSolicitacao({ setAvaliador }) {
   const [filters, setFilters] = useState({
@@ -25,6 +25,8 @@ export default function EnviarSolicitacao({ setAvaliador }) {
   });
 
   const [evaluators, setEvaluators] = useState([]);
+
+  const [solicitacao, setSolicitacao] = useState(false);
 
   async function search() {
     api.post(`users/search`, {
@@ -41,61 +43,66 @@ export default function EnviarSolicitacao({ setAvaliador }) {
 
   return (
     <>
-      <div className="col-12 title">
-        <h1>Novo Avaliador</h1>
-      </div>
-      <div className="col-12 px-0">
-        <Card className="red">
-          <div className="card-body">
-            <FiltersEvaluator filters={filters} setFilters={setFilters} setEvaluators={setEvaluators} only />
-
-            <Table>
-              <thead>
-                <tr>
-                  <th className="col-1">#</th>
-                  <th className="col-5">Nome</th>
-                  <th className="col-3">E-mail</th>
-                  {/* <th className="col-1">Cargo/Função</th> */}
-                  <th className="col-1">Avaliador</th>
-                  <th className="col-2">Ações</th>
-                </tr>
-              </thead>
-              <tbody>
-                {evaluators.map((item, index) => (
-                  <tr>
-                    <td style={{ textAlign: 'center' }}>{ (index + 1) }</td>
-                    <td style={{ textAlign: 'center' }}>{ item.name }</td>
-                    <td style={{ textAlign: 'center' }}>{ item.email }</td>
-                    {/* <td style={{ textAlign: 'center' }}>{ item.office_name }</td> */}
-                    <td style={{ textAlign: 'center' }}>{ item.evaluator ? 'SIM' : 'NÃO' }</td>
-                    <td style={{ textAlign: 'center' }}>
-                      <button
-                        data-tip="Enviar Solicitação"
-                        onClick={() => {}}
-                        className="edit"
-                      >
-                        <FiEdit />
-                      </button>
-                      <ReactTooltip />
-
-                    </td>
-                  </tr>
-                ))}
-              </tbody>
-            </Table>
-
-            <Form>
-              <StyledModal>
-                <div className="modal-footer">
-                  <button style={{ marginTop: 20 }} type="button" className="close" onClick={() => setAvaliador(false)}>
-                    Voltar
-                  </button>
-                </div>
-              </StyledModal>
-            </Form>
+      {!solicitacao ? (
+        <>
+          <div className="col-12 title">
+            <h1>Novo Avaliador</h1>
           </div>
-        </Card>
-      </div>
+          <div className="col-12 px-0">
+            <Card className="red">
+              <div className="card-body">
+                <FiltersEvaluator filters={filters} setFilters={setFilters} setEvaluators={setEvaluators} only />
+
+                <Table>
+                  <thead>
+                    <tr>
+                      <th className="col-1">#</th>
+                      <th className="col-5">Nome</th>
+                      <th className="col-3">E-mail</th>
+                      {/* <th className="col-1">Cargo/Função</th> */}
+                      <th className="col-1">Avaliador</th>
+                      <th className="col-2">Ações</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {evaluators.map((item, index) => (
+                      <tr>
+                        <td style={{ textAlign: 'center' }}>{ (index + 1) }</td>
+                        <td style={{ textAlign: 'center' }}>{ item.name }</td>
+                        <td style={{ textAlign: 'center' }}>{ item.email }</td>
+                        {/* <td style={{ textAlign: 'center' }}>{ item.office_name }</td> */}
+                        <td style={{ textAlign: 'center' }}>{ item.evaluator ? 'SIM' : 'NÃO' }</td>
+                        <td style={{ textAlign: 'center' }}>
+                          <button
+                            data-tip="Enviar Solicitação"
+                            onClick={() => setSolicitacao(true)}
+                            className="edit"
+                          >
+                            <FiEdit />
+                          </button>
+                          <ReactTooltip />
+                        </td>
+                      </tr>
+                    ))}
+                  </tbody>
+                </Table>
+
+                <Form>
+                  <StyledModal>
+                    <div className="modal-footer">
+                      <button style={{ marginTop: 20 }} type="button" className="close" onClick={() => setAvaliador(false)}>
+                        Voltar
+                      </button>
+                    </div>
+                  </StyledModal>
+                </Form>
+              </div>
+            </Card>
+          </div>
+        </>
+      ) : (
+        <Solicitacao setSolicitacao={setSolicitacao} />
+      )}
     </>
   );
 }
---
+++ src/pages/private/EvaluatorsProgram/Solicitação/index.js
@@ -0,0 +1,166 @@
+import React, { useCallback, useRef, useState } from 'react';
+
+import { FiFile, FiCheckCircle } from 'react-icons/fi';
+
+import ReactTooltip from 'react-tooltip';
+
+import { Form as Unform } from '@unform/web';
+
+import { store } from 'react-notifications-component';
+
+import * as Yup from 'yup';
+
+import Input from '../../../../components/Input';
+
+import { StyledModal } from './styles';
+import { Card } from '../../../../components/Card';
+import { Form } from '../../../../components/Form';
+
+import getValidationErrors from '../../../../utils/getValidationErrors';
+
+export default function Solicitacao({ setSolicitacao }) {
+  const formRef = useRef(null);
+  const reference = useRef(null);
+
+  const [selectedFile, setSelectedFile] = useState(null);
+  const [errorSubject, setErrorSubject] = useState('');
+
+  const [description, setDescription] = useState("");
+
+  const handleSubmit = useCallback(
+    async (data) => {
+      try {
+        formRef.current.setErrors({});
+
+        const schema = Yup.object().shape({
+          title: Yup.string().required('Campo obrigatório'),
+        });
+
+        if (!description) {
+          setErrorSubject('Campo obrigatório');
+        } else {
+          setErrorSubject('');
+        }
+
+        await schema.validate(data, {
+          abortEarly: false,
+        });
+
+        if (!description) {
+          throw 'Campo obrigatório';
+        }
+      } catch (error) {
+        if (error instanceof Yup.ValidationError) {
+          const errors = getValidationErrors(error);
+
+          formRef.current.setErrors(errors);
+        }
+      }
+    },
+    [formRef, description],
+  );
+
+  return (
+    <>
+      <div className="col-12 title">
+        <h1>Nova Solicitação</h1>
+      </div>
+      <div className="col-12 px-0">
+        <Card className="red">
+          <div className="card-body">
+            <Unform ref={formRef} onSubmit={handleSubmit}>
+
+              <Form>
+                <div className="modal-body" ref={reference}>
+                  <Input formRef={formRef} name="title" required original title="Assunto" />
+
+                  <div className="input-block">
+                    <label>
+                      Conteúdo
+                      <sup style={{ color: "#f00" }}>*</sup>
+                    </label>
+
+                    <textarea
+                      rows={5}
+                      style={{ borderColor: errorSubject ? "rgb(197, 48, 48)" : "rgb(153, 153, 153)", padding: 10 }}
+                      value={description}
+                      onChange={(e) => setDescription(e.target.value)}
+                      maxLength={255}
+                    />
+                    <div style={{ display: "flex", justifyContent: 'space-between', alignItems: 'center' }}>
+                      <sup style={{ color: '#c53030', marginTop: 5 }}>
+                        {errorSubject}
+                      </sup>
+                      <span style={{
+                        color: '#626262', justifyContent: 'flex-end', fontSize: 11,
+                      }}
+                      >
+                        {`${description.length}/255 caracteres`}
+                      </span>
+                    </div>
+
+                  </div>
+
+                  <div style={{ marginBottom: 10 }} className="input-block">
+                    <label htmlFor="email">
+                      Anexo
+                    </label>
+                    <div style={{ marginBottom: 5 }} />
+                    <label style={{ borderColor: "#dee2e6" }} className="file-input">
+                      <input
+                        type="file"
+                        placeholder="Arquivo"
+                        accept=".pdf"
+                        onChange={(e) => {
+                          if (e.target.files.length > 0) {
+                            if (e.target.files[0].size / 1000000 > 3) {
+                              store.addNotification({
+                                message: `Seu arquivo: ${e.target.files[0].name} é muito grande! Max:${3}MB`,
+                                type: 'danger',
+                                insert: 'top',
+                                container: 'top-right',
+                                animationIn: ['animate__animated', 'animate__fadeIn'],
+                                animationOut: ['animate__animated', 'animate__fadeOut'],
+                                dismiss: {
+                                  duration: 5000,
+                                  onScreen: true,
+                                },
+                              });
+                            } else {
+                              setSelectedFile(e.target.files[0]);
+                            }
+                          }
+                        }}
+                      />
+                      <div className="text">
+                        { selectedFile ? selectedFile.name : 'Selecione um arquivo' }
+                      </div>
+                      <div className="icon">
+                        <FiFile />
+                      </div>
+                    </label>
+                  </div>
+                </div>
+
+                <Form>
+                  <StyledModal>
+                    <div className="modal-footer">
+                      <button style={{ marginTop: 20 }} type="button" className="close" onClick={() => setSolicitacao(false)}>
+                        Voltar
+                      </button>
+                      <button type="submit" className="submit">
+                        <FiCheckCircle />
+                        {' '}
+                        Enviar
+                      </button>
+                    </div>
+                  </StyledModal>
+                </Form>
+              </Form>
+            </Unform>
+          </div>
+        </Card>
+      </div>
+    </>
+  );
+}
